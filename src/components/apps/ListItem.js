75a21ac7db1c4245408a84e5c034dcce0dc11e42
tawatchai.w <tawatchai.w@nextcorp.co.th>
2017-12-15 18:45:25 +0700

    tutorial

--- src/components/apps/ListItem.js
+++ src/components/apps/ListItem.js
@@ -46,7 +46,7 @@ export default class ListItem extends React.Component {
                     {/* <figure className="image is-128x128 is-center">
                         <img src="https://bulma.io/images/placeholders/128x128.png" />
                     </figure> */}
-                    <div className="content">
+                    {/* <div className="content">
                         {`const config ={ appId : '${this.state.appId}', 
                         appName:'${this.state.appName}', 
                         domainName:'${this.state.domainName}', 
@@ -54,6 +54,76 @@ export default class ListItem extends React.Component {
                         securityKey:'auth@kmutnb',
                         authDomain:'kmutnb-auth.herokuapp.com/callback/'
                         }`}
+                    </div> */}
+                    <div className="card">
+                        <div className="card-content">
+                            <p className="title">Node.js</p>
+                            <p className="subtitle">npm install jsonwebtoken</p>
+                        </div>
+                        <footer className="card-footer">
+                            <p className="card-footer-item">
+                                <code>const jwt = require('jsonwebtoken');
+                                <br />
+                                    {`const config ={ appId : '${this.state.appId}', 
+                        appName:'${this.state.appName}', 
+                        domainName:'${this.state.domainName}', 
+                        callbackUrl:'${this.state.callbackUrl}',
+                        securityKey:'auth@kmutnb',
+                        authDomain:'kmutnb-auth.herokuapp.com/callback/'
+                        }`}
+                        <br />
+                        {
+                            'jwt.verify(request.query.token, config.securityKey, function (err, decoded) {'+
+                                'if (err) {'+
+                                    'response.redirect(`http://${config.authDomain}${config.callbackUrl}/${config.appId}`);'+
+                                '} else {'+
+                                    'if (decoded.appId === config.appId) {'+
+                                        'response.redirect(`/?token=${request.query.token}`);'+
+                                    '} else {'+
+                                        'response.redirect(`http://${config.authDomain}${config.callbackUrl}/${config.appId}`);'+
+                                    '}'+
+                               '}'+
+                            '});'
+                        }
+                                </code>
+                            </p>
+                        </footer>
+                    </div>
+                    <div className="card">
+                        <div className="card-content">
+                            <p className="title">PHP</p>
+                            <p className="subtitle"><a href="https://github.com/firebase/php-jwt">composer require firebase/php-jwt</a></p>
+                        </div>
+                        <footer className="card-footer">
+                            <p className="card-footer-item">
+                                <code>
+                                {
+                                    `<?php
+                                        use \Firebase\JWT\JWT;
+
+
+                                        $config = array(
+                                            "appId" => "-L0Oosr9HzBOfUP2kOdW",
+                                            "appName"=>"สวัสดิการ",
+                                            "domainName"=>"localhost:3000", 
+                                            "callbackUrl"=>"localhost:3000/api/auth",
+                                            "securityKey"=>"auth@kmutnb",
+                                            "authDomain"=>"kmutnb-auth.herokuapp.com/callback/"
+                                        );
+
+                                        $decoded = JWT::decode($_GET["token"], $config["securityKey"], array('HS256'));
+
+                                        if($decoded["appId"]===$config["appId"]){
+                                            echo 'authen ok';
+                                        }else{
+                                            echo 'authen fail';
+                                        }
+
+                                        ?>`
+                                    }
+                                </code>
+                            </p>
+                        </footer>
                     </div>
                     {/* <div className="field">
                         {
